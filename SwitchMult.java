e43c00e634a4851a8fb313575469d037e3261779
piecucci <piecucci@gmail.com>
2024-04-22 12:55:40 +0200

    Add files via upload

---
+++ SwitchMult.java
@@ -0,0 +1,13 @@
+// ACF-SwitchMult/SwitchMult.java
+ 
+public class SwitchMult {
+    public static void main(String[] args) {
+        String n = "BMW", country = null;
+        switch (n) {
+            case "BMW", "Opel", "Audi" -> country = "Germany";
+            case "Peugeot", "Citroen"  -> country = "France";
+            default                    -> country = "unknown";
+        }
+        System.out.println(n + " -> " + country);
+    }
+}
